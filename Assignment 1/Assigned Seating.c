b73aa33a0b274093fe8e75255b2039c06c8c4ca5
Giovanni Acireale <gio.acireale@gmail.com>
2023-09-25 21:02:51 -0400

    Assignment 1

--- Assignment 1/Assigned Seating.c
+++ Assignment 1/Assigned Seating.c
@@ -39,3 +39,144 @@ unowned.
 
 */
 
+typedef struct Seat Seat;
+typedef struct Row Row;
+
+// a row will consist of a number and a list of seats per number
+struct Row {
+	int rowNum;
+	Seat* seatList;
+	Row* next;
+};
+
+// a seat will consist of a number and a name
+struct Seat {
+	int seatNum;
+	char* name;
+	Seat* next;
+};
+
+// function prototypes
+void buy(Row* rowList, int row, int start, int end, char* name);
+void lookup(Row* rowList, int row, int seat);
+void quit(Row* rowList);
+
+// buy seats
+void buy(Row* rowList, int row, int start, int end, char* name) {
+	// check if row exists
+	Row* currentRow = rowList->next;
+	
+	// if row list is empty, create new row
+	if (currentRow == NULL) {
+		Row* newRow = (Row*)malloc(sizeof(Row));
+		newRow->rowNum = row;
+		newRow->next = NULL;
+		rowList->next = newRow;
+		currentRow = newRow;
+	}
+	
+	// if row exists, look for each seat between start and end
+	while (currentRow->next != NULL) {
+		if (currentRow->rowNum == row) {
+			Seat* currentSeat = currentRow->seatList;
+			while (currentSeat->next != NULL) {
+				if (currentSeat->seatNum >= start && currentSeat->seatNum <= end) {
+					printf("FAILURE\n");
+					return;
+				}
+				currentSeat = currentSeat->next;
+			}
+		}
+		currentRow = currentRow->next;
+	}
+	
+	// if row does not exist, create new row
+	Row* newRow = (Row*)malloc(sizeof(Row));
+	newRow->rowNum = row;
+	newRow->next = NULL;
+	currentRow->next = newRow;
+	currentRow = newRow;
+	
+	// create new seats
+	for (int i = start; i <= end; i++) {
+		Seat* newSeat = (Seat*)malloc(sizeof(Seat));
+		newSeat->seatNum = i;
+		newSeat->name = name;
+		newSeat->next = NULL;
+		currentRow->seatList->next = newSeat;
+		currentRow->seatList = newSeat;
+	}
+	
+	printf("SUCCESS\n");
+	return;
+}
+
+// lookup seat
+void lookup(Row* rowList, int row, int seat) {
+	// if row list is empty, return no one
+	if (rowList->next == NULL) {
+		printf("No one\n");
+		return;
+	}
+
+	// if row exists, look for seat
+	else {
+		Row* currentRow = rowList->next;
+		while (currentRow->next != NULL) {
+			if (currentRow->rowNum == row) {
+				Seat* currentSeat = currentRow->seatList;
+				while (currentSeat->next != NULL) {
+					if (currentSeat->seatNum == seat) {
+						printf("%s\n", currentSeat->name);
+						return;
+					}
+					currentSeat = currentSeat->next;
+				}
+			}
+			currentRow = currentRow->next;
+		}
+	}
+
+	// if seat is not taken, return no one
+	printf("No one\n");
+}
+
+// quit program
+void quit(Row* rowList) {
+	// free memory
+	free(rowList);
+}
+
+int main() {
+	// create row list
+	Row* rowList = (Row*)malloc(sizeof(Row));
+	rowList->next = NULL;
+
+	// read input
+	char input[100];
+	while (fgets(input, 100, stdin) != NULL) {
+		// parse input
+		char* command = strtok(input, " ");
+		int row = atoi(strtok(NULL, " "));
+		int start = atoi(strtok(NULL, " "));
+		int end = atoi(strtok(NULL, " "));
+		char* name = strtok(NULL, " ");
+
+		// remove newline from name
+		name[strlen(name) - 1] = '\0';
+
+		// execute command
+		if (strcmp(command, "BUY") == 0) {
+			buy(rowList, row, start, end, name);
+		}
+		else if (strcmp(command, "LOOKUP") == 0) {
+			lookup(rowList, row, start);
+		}
+		else if (strcmp(command, "QUIT") == 0) {
+			quit(rowList);
+			return 0;
+		}
+	}
+
+	return 0;
+}
