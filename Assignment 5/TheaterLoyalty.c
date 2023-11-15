a2fe277285616211f0d873bb0dc2a3e6162b2e56
Giovanni Acireale <gio.acireale@gmail.com>
2023-11-15 00:01:02 -0500

    Assignment 5 submission
    there is still some errors regarding the creation of the tree and the calculation of the depth value.

--- Assignment 5/TheaterLoyalty.c
+++ Assignment 5/TheaterLoyalty.c
@@ -38,386 +38,232 @@ Sort.
 #include <stdlib.h>
 #include <string.h>
 
-// Struct for the customer
 typedef struct customer {
-	char *name;
-	int points;
-	struct customer *left;
-	struct customer *right;
+    char* name;
+    int points;
+    int depth;
+    struct customer* left;
+    struct customer* right;
 } customer;
 
 // Function prototypes
-customer *addCustomer(customer *root, char *name, int points);
-customer *deleteCustomer(customer *root, char *name);
-customer *findCustomer(customer *root, char *name);
-customer *findMin(customer *root);
-int countCustomers(customer *root, char *name);
-void printCustomers(customer *root);
-void freeCustomers(customer *root);
+customer* addCustomer(customer* root, char* name, int points, int depth);
+customer* deleteCustomer(customer* root, char* name, int* depth);
+customer* findCustomer(customer* root, char* name, int* depth);
+customer* findMin(customer* root);
+int countCustomers(customer* root, char* name);
+void printCustomers(customer* root);
+void freeCustomers(customer* root);
+void updateDepth(customer* root, int depth);
 
 // Main function
 int main() {
-	// Initialize variables
-	int points, depth, count;
-	char name[100];
-	char command[100];
-	customer *root = NULL, *temp = NULL;
+    int count, points, depth = 0;
+    char name[100];
+    char command[100];
+    customer* root = NULL, * temp = NULL;
 
-	/*
-	Input
-	The first line of input contains a single positive integer: n (n ? 300,000), the number of
-	commands to process.
-	The next n lines will each contain a single command. Note: The commands will be such that
-	the resulting binary search tree will never exceed a height of 100.
-	Here is the format of each of the possible input lines:
-	Command 1
-	add <name> <points>
-	<name> will be a lowercase alphabetic string with no more than 19 characters.
-	<points> will be a positive integer less than or equal to 100.
-	Command 2
-	sub <name> <points>
-	<name> will be a lowercase alphabetic string with no more than 19 characters.
-	<points> will be a positive integer less than or equal to 100.
-	Note: if a customer has fewer points than is specified in this command to subtract, then just
-	subtract the total number of points they have instead.
-	Command 3
-	del <name>
-	<name> will be a lowercase alphabetic string with no more than 19 characters.
-	Delete the customer with the name <name> from the binary search tree. No action is taken if
-	the customer isn't in the tree to begin with.
-	Command 4
-	search <name>
-	<name> will be a lowercase alphabetic string with no more than 19 characters.
-	This will search for the customer with the name <name> and report both the number of loyalty
-	points the customer has and the depth of the node in the tree storing that customer, if the
-	customer is in the tree.
-	Command 5
-	count_smaller <name>
-	<name> will be a lowercase alphabetic string with no more than 19 characters.
-	This will calculate the number of names in the binary search tree that come alphabetically before
-	<name>.
-	
-	Output
-	For each input command, output a single line as described below:
-	Command 1
-	Print out a single line with the format:
-	<name> <points>
-	where <name> is the name of the customer who added points and <points> is the new total
-	number of points they have. Note: If the customer is not in the binary search tree, then create a
-	new node for them initialized to <points> number of points. If the customer is already in the
-	binary search tree, just add <points> number of points to their current total.
-	Command 2
-	If the customer is NOT in the tree, print out a single line with the format:
-	<name> not found
-	If the customer is in the tree, print out a single line with the format:
-	<name> <points>
-	where <name> is the name of the customer who subtracted points and <points> is the new
-	total number of points they have. Note: if the points to be subtracted was greater than the points
-	they previously had, then after the operation, they will have 0 points left.
-	Command 3
-	If the customer in question wasn't found in the binary search tree, output the following line:
-	<name> not found
-	If the name is found, output a line with the following format:
-	<name> deleted
-	where <name> is the name of the customer being deleted. (Of course, delete the node storing
-	that customer from the tree!) If you are deleting a node with two children, please replace it
-	with the maximum node in the left subtree. This is to ensure there is one right answer for
-	each test case.
-	Command 4
-	If the customer in question wasn't found in the binary search tree, output the following line:
-	<name> not found
-	If the name is found, output a line with the following format:
-	<name> <points> <depth>
-	where <name> is the name of the customer being searched, <points> is the number of
-	loyalty points they currently have and <depth> is the distance of the node the customer in
-	question was found in from the root node of the tree.
-	Command 5
-	For this command, just print a single integer on a line by itself representing the number of names
-	in the binary search tree that come before <name>, alphabetically. (Note: Because we require a
-	run time of O(h), where h is the height of the tree, this is likely the most challenging command to
-	process. The size field will be necessary in the node struct to make this determination in O(h)
-	time.)
-	After all commands in the input have been processed, create an array to store pointers to each
-	struct storing customer data. Then, sort that array by customer loyalty points from highest to
-	lowest, breaking ties by the names in alphabetical order as previously described. Finally, print
-	out one line per customer in this sorted order with the format:
-	<name> <points>
-	where <name> is the name of the customer and <points> is the number of loyalty points
-	they have at the end of the set of input commands.
-	*/
+    scanf("%d", &count);
 
-	// Scan the number of commands
-	scanf("%d", &count);
+    for (int i = 0; i < count; i++) {
+        scanf("%s", command);
 
-	// For each command
-	for (int i = 0; i < count; i++) {
-		// Scan the command
-		scanf("%s", command);
-		// If the command is add
-		if (strcmp(command, "add") == 0) {
-			// Scan the name and points
-			scanf("%s %d", name, &points);
-			// Add the customer
-			root = addCustomer(root, name, points);
-			// Print the customer
-			printf("%s %d\n", name, points);
-			// If the command is sub
-		}
-		else if (strcmp(command, "sub") == 0) {
-			// Scan the name and points
-			scanf("%s %d", name, &points);
-			// Find the customer
-			temp = findCustomer(root, name);
-			// If the customer is NULL
-			if (temp == NULL) {
-				// Print the customer not found
-				printf("%s not found\n", name);
-				// If the customer is not NULL
-			}
-			else {
-				// Subtract the points
-				temp->points -= points;
-				// If the points are less than 0
-				if (temp->points < 0) {
-					// Set the points to 0
-					temp->points = 0;
-				}
-				// Print the customer
-				printf("%s %d\n", name, temp->points);
-			}
-			// If the command is del
-		}
-		else if (strcmp(command, "del") == 0) {
-			// Scan the name
-			scanf("%s", name);
-			// Delete the customer
-			root = deleteCustomer(root, name);
-			// Print the customer
-			printf("%s deleted\n", name);
-			// If the command is search
-		}
-		else if (strcmp(command, "search") == 0) {
-			// Scan the name
-			scanf("%s", name);
-			// Find the customer
-			temp = findCustomer(root, name);
-			// If the customer is NULL
-			if (temp == NULL) {
-				// Print the customer not found
-				printf("%s not found\n", name);
-				// If the customer is not NULL
-			}
-			else {
-				// Count the customers
-				depth = countCustomers(root, name);
-				// Print the customer
-				printf("%s %d %d\n", name, temp->points, depth);
-			}
-			// If the command is count_smaller
-		}
-		else if (strcmp(command, "count_smaller") == 0) {
-			// Scan the name
-			scanf("%s", name);
-			// Count the customers
-			count = countCustomers(root, name);
-			// Print the count
-			printf("%d\n", count);
-		}
-	}
+        if (strcmp(command, "add") == 0) {
+            scanf("%s %d", name, &points);
+            root = addCustomer(root, name, points, 0);
+            temp = findCustomer(root, name, NULL);
+            printf("%s %d\n", name, temp->points);
+        }
+        else if (strcmp(command, "sub") == 0) {
+            scanf("%s %d", name, &points);
+            temp = findCustomer(root, name, NULL);
+            if (temp == NULL) {
+                printf("%s not found\n", name);
+            }
+            else {
+                temp->points -= points;
+                if (temp->points < 0) {
+                    temp->points = 0;
+                }
+                printf("%s %d\n", name, temp->points);
+            }
+        }
+        else if (strcmp(command, "del") == 0) {
+            scanf("%s", name);
+            root = deleteCustomer(root, name, &depth);
+            printf("%s deleted\n", name);
+        }
+        else if (strcmp(command, "search") == 0) {
+            scanf("%s", name);
+            temp = findCustomer(root, name, &depth);
+            if (temp == NULL) {
+                printf("%s not found\n", name);
+            }
+            else {
+                printf("%s %d %d\n", name, temp->points, depth);
+            }
+        }
+        else if (strcmp(command, "count_smaller") == 0) {
+            scanf("%s", name);
+            int count = countCustomers(root, name);
+            printf("%d\n", count);
+        }
+    }
 
-	// Print the customers
-	printCustomers(root);
+    printCustomers(root);
+    freeCustomers(root);
 
-	// Free the customers
-	freeCustomers(root);
-
-	// Return 0
-	return 0;
+    return 0;
 }
 
-// Function to add a customer
-customer *addCustomer(customer *root, char *name, int points) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Allocate memory for the customer
-		root = malloc(sizeof(customer));
-		// Allocate memory for the name
-		root->name = malloc(sizeof(char) * (strlen(name) + 1));
-		// Copy the name
-		strcpy(root->name, name);
-		// Set the points
-		root->points = points;
-		// Set the left and right to NULL
-		root->left = NULL;
-		root->right = NULL;
-		// Return the root
-		return root;
-	}
-	// If the name is less than the root's name
-	if (strcmp(name, root->name) < 0) {
-		// Add the customer to the left
-		root->left = addCustomer(root->left, name, points);
-	// If the name is greater than the root's name
-	} else if (strcmp(name, root->name) > 0) {
-		// Add the customer to the right
-		root->right = addCustomer(root->right, name, points);
-	// If the name is equal to the root's name
-	} else {
-		// Add the points
-		root->points += points;
-	}
-	// Return the root
-	return root;
+customer* addCustomer(customer* root, char* name, int points, int depth) {
+    if (root == NULL) {
+        root = malloc(sizeof(customer));
+        root->name = malloc(sizeof(char) * (strlen(name) + 1));
+        strcpy(root->name, name);
+        root->points = points;
+        root->depth = depth;
+        root->left = NULL;
+        root->right = NULL;
+    }
+    else if (strcmp(name, root->name) < 0) {
+        root->left = addCustomer(root->left, name, points, depth + 1);
+    }
+    else if (strcmp(name, root->name) > 0) {
+        root->right = addCustomer(root->right, name, points, depth + 1);
+    }
+    else {
+        root->points += points;
+    }
+
+    return root;
 }
 
-// Function to delete a customer
-customer *deleteCustomer(customer *root, char *name) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Return NULL
-		return NULL;
-	}
-	// If the name is less than the root's name
-	if (strcmp(name, root->name) < 0) {
-		// Delete the customer from the left
-		root->left = deleteCustomer(root->left, name);
-	// If the name is greater than the root's name
-	} else if (strcmp(name, root->name) > 0) {
-		// Delete the customer from the right
-		root->right = deleteCustomer(root->right, name);
-	// If the name is equal to the root's name
-	} else {
-		// If the root has no children
-		if (root->left == NULL && root->right == NULL) {
-			// Free the name
-			free(root->name);
-			// Free the root
-			free(root);
-			// Set the root to NULL
-			root = NULL;
-		// If the root has a left child
-		} else if (root->left != NULL && root->right == NULL) {
-			// Free the name
-			free(root->name);
-			// Free the root
-			free(root);
-			// Set the root to the left child
-			root = root->left;
-		// If the root has a right child
-		} else if (root->left == NULL && root->right != NULL) {
-			// Free the name
-			free(root->name);
-			// Free the root
-			free(root);
-			// Set the root to the right child
-			root = root->right;
-		// If the root has two children
-		} else {
-			// Find the minimum node in the right subtree
-			customer *temp = findMin(root->right);
-			// Free the name
-			free(root->name);
-			// Copy the name
-			root->name = malloc(sizeof(char) * (strlen(temp->name) + 1));
-			strcpy(root->name, temp->name);
-			// Set the points
-			root->points = temp->points;
-			// Delete the minimum node in the right subtree
-			root->right = deleteCustomer(root->right, temp->name);
-		}
-	}
-	// Return the root
-	return root;
+customer* deleteCustomer(customer* root, char* name, int* depth) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (strcmp(name, root->name) < 0) {
+        (*depth)++;
+        root->left = deleteCustomer(root->left, name, depth);
+    }
+    else if (strcmp(name, root->name) > 0) {
+        (*depth)++;
+        root->right = deleteCustomer(root->right, name, depth);
+    }
+    else {
+        if (root->left == NULL) {
+            customer* temp = root->right;
+            free(root->name);
+            free(root);
+
+            // Adjust depth for the new root
+            if (temp != NULL) {
+                temp->depth = 0;
+            }
+
+            return temp;
+        }
+        else if (root->right == NULL) {
+            customer* temp = root->left;
+            free(root->name);
+            free(root);
+
+            // Adjust depth for the new root
+            if (temp != NULL) {
+                temp->depth = 0;
+            }
+
+            return temp;
+        }
+
+        // Find the minimum node in the right subtree
+        customer* temp = findMin(root->right);
+        free(root->name);
+        root->name = malloc(sizeof(char) * (strlen(temp->name) + 1));
+        strcpy(root->name, temp->name);
+        root->points = temp->points;
+
+        // Delete the minimum node in the right subtree
+        root->right = deleteCustomer(root->right, temp->name, depth);
+    }
+
+    return root;
 }
 
-// Function to find a customer
-customer *findCustomer(customer *root, char *name) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Return NULL
-		return NULL;
-	}
-	// If the name is less than the root's name
-	if (strcmp(name, root->name) < 0) {
-		// Find the customer in the left subtree
-		return findCustomer(root->left, name);
-	// If the name is greater than the root's name
-	} else if (strcmp(name, root->name) > 0) {
-		// Find the customer in the right subtree
-		return findCustomer(root->right, name);
-	// If the name is equal to the root's name
-	} else {
-		// Return the root
-		return root;
-	}
+customer* findCustomer(customer* root, char* name, int* depth) {
+    if (root == NULL) {
+        if (depth != NULL) {
+            *depth = -1;
+        }
+        return NULL;
+    }
+
+    if (strcmp(name, root->name) < 0) {
+        if (depth != NULL) {
+            *depth = root->depth;
+        }
+        return findCustomer(root->left, name, depth);
+    }
+    else if (strcmp(name, root->name) > 0) {
+        if (depth != NULL) {
+            *depth = root->depth;
+        }
+        return findCustomer(root->right, name, depth);
+    }
+    else {
+        if (depth != NULL) {
+            *depth = root->depth;
+        }
+        return root;
+    }
 }
 
-// Function to find the minimum node in the right subtree
-customer *findMin(customer *root) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Return NULL
-		return NULL;
-	}
-	// If the root has no left child
-	if (root->left == NULL) {
-		// Return the root
-		return root;
-	}
-	// Find the minimum node in the left subtree
-	return findMin(root->left);
+customer* findMin(customer* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
 }
 
-// Function to count the number of customers before a customer
-int countCustomers(customer *root, char *name) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Return 0
-		return 0;
-	}
-	// If the name is less than the root's name
-	if (strcmp(name, root->name) < 0) {
-		// Count the customers in the left subtree
-		return countCustomers(root->left, name);
-	// If the name is greater than the root's name
-	} else if (strcmp(name, root->name) > 0) {
-		// Count the customers in the left subtree, the root, and the right subtree
-		return 1 + countCustomers(root->left, name) + countCustomers(root->right, name);
-	// If the name is equal to the root's name
-	} else {
-		// Count the customers in the left subtree
-		return countCustomers(root->left, name);
-	}
+int countCustomers(customer* root, char* name) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (strcmp(name, root->name) <= 0) {
+        return countCustomers(root->left, name);
+    }
+    else {
+        return 1 + countCustomers(root->right, name) + countCustomers(root->left, name);
+    }
 }
 
-// Function to print the customers
-void printCustomers(customer *root) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Return
-		return;
-	}
-	// Print the left subtree
-	printCustomers(root->left);
-	// Print the name and points
-	printf("%s %d\n", root->name, root->points);
-	// Print the right subtree
-	printCustomers(root->right);
+void printCustomers(customer* root) {
+    if (root != NULL) {
+        printCustomers(root->left);
+        printf("%s %d\n", root->name, root->points);
+        printCustomers(root->right);
+    }
 }
 
-// Function to free the customers
-void freeCustomers(customer *root) {
-	// If the root is NULL
-	if (root == NULL) {
-		// Return
-		return;
-	}
-	// Free the left subtree
-	freeCustomers(root->left);
-	// Free the right subtree
-	freeCustomers(root->right);
-	// Free the name
-	free(root->name);
-	// Free the root
-	free(root);
+void freeCustomers(customer* root) {
+    if (root != NULL) {
+        freeCustomers(root->left);
+        freeCustomers(root->right);
+        free(root->name);
+        free(root);
+    }
 }
+
+void updateDepth(customer* root, int depth) {
+    if (root != NULL) {
+        root->depth = depth;
+        updateDepth(root->left, depth + 1);
+        updateDepth(root->right, depth + 1);
+    }
+}
+
+
+
